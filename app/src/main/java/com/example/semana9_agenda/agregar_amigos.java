41e02abd9f6001d619bf62e0ca6c1d8813a1d220
Estela25 <USIS035821@ugb.edu.sv>
2022-04-06 17:13:57 -0500

    arreglos

--- app/src/main/java/com/example/semana9_agenda/agregar_amigos.java
+++ app/src/main/java/com/example/semana9_agenda/agregar_amigos.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 
 import android.view.View;
 
+import android.widget.Button;
 import android.widget.TextView;
 
 import android.widget.Toast;
@@ -27,6 +28,18 @@ public class agregar_amigos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_amigos);
         mostrardatos();
+
+
+        Button info = findViewById(R.id.btBase2);
+        info.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), vista.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
     public void mostrardatos(){
 
